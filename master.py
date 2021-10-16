ebd7d0623fa158fe8eaec1de78124ff138a6471d
mcgovernh <36536328+mcgovernh@users.noreply.github.com>
2021-10-16 22:12:08 +0100

    Update master.py

--- master.py
+++ master.py
@@ -4,10 +4,17 @@ import random
 
 app = Flask(__name__)
 
-# list of cat images
+# list of ireland images
 images = [
-    "images/AirCorp.jpg",
-    "images/Blaskets.png",
+    './drive/MyDrive/imagesofireland/cliffsofmoher.jpg',
+    './drive/MyDrive/imagesofireland/DesertedVillage.jpg',
+    './drive/MyDrive/imagesofireland/AirCorp.jpg',
+    './drive/MyDrive/imagesofireland/Derrynane.jpg',
+     './drive/MyDrive/imagesofireland/Blaskets.jpg',
+     './drive/MyDrive/imagesofireland/BrazenHeadDublin.jpg',
+     './drive/MyDrive/imagesofireland/cashel.jpg',
+     './drive/MyDrive/imagesofireland/dublincastle.jpg',
+     './drive/MyDrive/imagesofireland/castle.jpg'
 ]
 
 

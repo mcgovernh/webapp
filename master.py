08b55bf36d9d4c99e75e4082e8725992fb303bdd
mcgovernh <36536328+mcgovernh@users.noreply.github.com>
2021-10-16 22:25:42 +0100

    Update master.py

--- master.py
+++ master.py
@@ -6,15 +6,9 @@ app = Flask(__name__)
 
 # list of ireland images
 images = [
-    './drive/MyDrive/imagesofireland/cliffsofmoher.jpg',
-    './drive/MyDrive/imagesofireland/DesertedVillage.jpg',
-    './drive/MyDrive/imagesofireland/AirCorp.jpg',
-    './drive/MyDrive/imagesofireland/Derrynane.jpg',
-     './drive/MyDrive/imagesofireland/Blaskets.jpg',
-     './drive/MyDrive/imagesofireland/BrazenHeadDublin.jpg',
-     './drive/MyDrive/imagesofireland/cashel.jpg',
-     './drive/MyDrive/imagesofireland/dublincastle.jpg',
-     './drive/MyDrive/imagesofireland/castle.jpg'
+    'http://drive.google.com/uc?export=view&id=14_kMj43xi5YJpPbAaGeRgX9jI7b_btkC',
+    'http://drive.google.com/uc?export=view&id=14_kMj43xi5YJpPbAaGeRgX9jI7b_btkC',
+    'http://drive.google.com/uc?export=view&id=14_kMj43xi5YJpPbAaGeRgX9jI7b_btkC'
 ]
 
 

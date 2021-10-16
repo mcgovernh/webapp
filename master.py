d1b24f8d5005abb6f567de066ddc42add3a7ccb0
mcgovernh <36536328+mcgovernh@users.noreply.github.com>
2021-10-16 21:07:19 +0100

    Update master.py

--- master.py
+++ master.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://drive.google.com/file/d/14_kMj43xi5YJpPbAaGeRgX9jI7b_btkC/view?usp=sharing",
-    "https://drive.google.com/file/d/1XIiMMtESAUhXVmBvqMjs4jt8HEe7H7MX/view?usp=sharing",
+    "images/AirCorp.jpg",
+    "images/Blaskets.png",
 ]
 
 
